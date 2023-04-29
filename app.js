dbf770cf7b5851ef965cb51de534edf3c8ae72c8
misterOumar <oumarweb01@gmail.com>
2023-04-29 02:43:39 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let advice_id = document.querySelector(".advice_number");
+let advice_content = document.querySelector(".advice");
+const advice_generator = document.querySelector(".advice_generator");
+const api_link = "https://api.adviceslip.com/advice";
+
+function randomAdvice() {
+  fetch(api_link)
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      advice_id.innerText = result.slip.id;
+      advice_content.innerText = result.slip.advice;
+    });
+}
+
+advice_generator.addEventListener("click", randomAdvice);
+
+randomAdvice();
